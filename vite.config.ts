90986fd08b3398db3e778c50c6287e024a2a1e99
Lucas Mza <lucas.umeza@gmail.com>
2026-06-25 23:09:37 -0300

    Image fixed 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/vogue-web-base/",
+  base: process.env.NODE_ENV === 'production' ? '/vogue-web-base/' : '/',
   plugins: [react()],
 });
